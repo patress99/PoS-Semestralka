4cf685e3e2223397fd2fac9c0805e150be9cdb69
Drawethree <janci.kluka@gmail.com>
2021-01-03 15:19:57 +0100

    Basic client-server connection

--- main.cpp
+++ main.cpp
@@ -1,22 +1,40 @@
-#include <SFML/Graphics.hpp>
+// CREDIT https://www.youtube.com/watch?v=OSwutjvNjK4&fbclid=IwAR22EkUU9j1yGgXGYZJqERPBUjZdBo5yoA3g6SIhE9Afv-Rb6c0V84Hz1Y0&ab_channel=CodingMadeEasy
+#include<SFML/Network.hpp>
+#include<string>
+#include<iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(500, 500), "");
-    sf::CircleShape shape(250.f);
+using namespace std;
 
-    shape.setFillColor(sf::Color::Green);
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        window.draw(shape);
-        window.display();
+int main() {
+
+    sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+    sf::TcpSocket socket;
+    char connType;
+    char buffer[2000];
+
+    size_t received;
+    string text = "Connected to: ";
+
+    cout << "Enter (s) for Server, (c) for client: " << endl;
+
+    cin >> connType;
+
+    if (connType == 's') {
+        sf::TcpListener listener;
+        listener.listen(25565);
+        listener.accept(socket);
+        text+= "Server";
+    } else if (connType == 'c') {
+        socket.connect(ip,25565);
+        text += "Client";
     }
+
+    socket.send(text.c_str(),text.length() + 1);
+    socket.receive(buffer, sizeof(buffer), received);
+
+    cout << buffer << endl;
+
+    system("pause");
+
     return 0;
 }
