455fe786defa1363821e712e71c27dbd32ebdedd
Drawethree <janci.kluka@gmail.com>
2021-01-03 19:44:05 +0100

    Tons of changes

--- Hrac.h
+++ Hrac.h
@@ -1,24 +1,43 @@
 #include <SFML/Graphics.hpp>
+#include <string>
 
 class Hrac {
 
 private:
-    float positionX;
-    float positionY;
+    std::string name;
+    float movementSpeed;
+
+    float attackCooldown;
+    float attackCooldownMax;
+
     int health;
+    int maxHealth;
+
     sf::Sprite sprite;
     sf::Texture texture;
+
 public:
+
     ~Hrac();
-    Hrac(float spawnX, float spawnY);
-    void setHealth(int health);
-    const int getHealth();
-    void moveRight();
-    void moveLeft();
-    void jump();
-    const float getPositionX();
-    const float getPositionY();
-    void setPositionX(float pos);
-    void setPositionY(float pos);
-    sf::Sprite& getSprite();
+    Hrac(int player, std::string name, float spawnX, float spawnY);
+
+    const sf::Vector2f& getPos() const;
+    const sf::FloatRect getBounds() const;
+
+    const int getHealth() const;
+    const int getMaxHealth() const;
+    const std::string getName() const;
+
+    void setPosition(const sf::Vector2f pos);
+    void setPosition(const float x, const float y);
+    void setHealth(const int hp);
+    void loseHp(const int value);
+
+    void move(const float dirX, const float dirY);
+    const bool canAttack();
+
+    void updateAttack();
+    void update();
+    void render(sf::RenderTarget& target);
+
 };
