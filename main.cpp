fd6cbc032da7d2f7177a4fe109838d9f1a4c0bba
Drawethree <janci.kluka@gmail.com>
2021-01-03 14:42:14 +0100

    CMake with SFML setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(500, 500), "");
+    sf::CircleShape shape(250.f);
+
+    shape.setFillColor(sf::Color::Green);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
     return 0;
 }
