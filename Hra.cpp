f2b12b28a38a198158d285c873122b352d4cc211
Juraj Šibík <sibikjuraj@gmail.com>
2021-01-03 14:52:37 +0100

    vytvoril som triedu Hra

---
+++ Hra.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Juro on 2. 1. 2021.
+//
+
+#include "Hra.h"
+
+using namespace std;
+
+
+Hra::Hra() {
+    cout << "Stevo" << endl;
+
+
+}
+
+Hra::~Hra() {
+
+
+}
---
+++ Hra.h
@@ -0,0 +1,21 @@
+//
+// Created by Juro on 2. 1. 2021.
+//
+#include <iostream>
+#ifndef POS_SEMKA_HRA_H
+#define POS_SEMKA_HRA_H
+
+
+
+class Hra {
+
+
+    public:
+        Hra();
+        ~Hra();
+
+
+};
+
+
+#endif //POS_SEMKA_HRA_H
